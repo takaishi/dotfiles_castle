3afa1f569f0e51a4aadbddad21dfae0a66cdf8f2
r_takaishi <ryo.takaishi.0@gmail.com>
2017-05-03 14:32:02 +0900

    auto install fisherman and plugins

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,11 @@
 
-fisher z
-fisher omf/theme-clearance
-fisher omf/plugin-peco
-fisher yoshiori/fish-peco_select_ghq_repository
+if not test -e ~/.config/fish/functions/fisher.fish
+  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+  fisher z
+  fisher omf/theme-clearance
+  fisher omf/plugin-peco
+  fisher yoshiori/fish-peco_select_ghq_repository
+end
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
