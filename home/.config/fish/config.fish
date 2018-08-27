92b914b51e44585ad41a6bab45b746edbdb86eaa
Ryo TAKAISHI <ryo.takaishi.0@gmail.com>
2018-08-27 17:02:14 +0900

    enable fzf & disable peco

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,5 @@
 set -x PATH $HOME/.rbenv/bin $PATH
 
-#peco
-function fish_user_key_bindings
-    bind \cr peco_select_history
-end
-
 # alias
 alias gl "cd (ghq root)/(ghq list | peco)"
 alias gd "git diff --color -w"
@@ -14,8 +9,6 @@ if test (which colordiff)
 else
   alias diff "diff -u"
 end
-
-
 
 if type -q direnv
   function __direnv_export_eval --on-variable PWD
@@ -34,10 +27,6 @@ end
 function peco_sync_select_history
   history-merge
   peco_select_history $argv
-end
-
-function fish_user_key_bindings
-  bind \cr 'peco_sync_select_history (commandline -b)'
 end
 
 function refresh-tmux --on-event fish_preexec
