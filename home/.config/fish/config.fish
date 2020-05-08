d7b9d10054fcbd8460bddc832bd032521396d4f7
r_takaishi <ryo.takaishi.0@gmail.com>
2020-05-08 10:53:23 +0900

    use static path for speed up

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,15 +1,31 @@
+#!/usr/local/bin/fish
+#
+
 set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH $HOME/bin $PATH
 set -x PATH /usr/local/kubebuilder/bin/ $PATH
+set -x PATH /usr/local/opt/python/libexec/bin $PATH
+set -x PATH $HOME/Library/Python/3.7/bin $PATH
+set -x PATH $HOME/.nodebrew/current/bin $PATH
+set -x PATH /usr/local/opt/gnu-time/libexec/gnubin $PATH
+set -x PATH $HOME/.krew/bin $PATH
+
+
+
 set -x GOPATH $HOME
 set -x GO111MODULE on
 set -x PATH /usr/local/opt/libxml2/bin $PATH
 set -x LANG en_US.UTF-8
+set -x PATH /usr/local/opt/avr-gcc@8/bin:$PATH
+set -x PATH /usr/local/opt/gnu-sed/libexec/gnubin:$PATH
+set -x DYLD_LIBRARY_PATH /usr/local/Cellar/openssl/1.0.2s/lib
+
 
 set -x LDFLAGS -L/usr/local/opt/libxml2/lib
 set -x CPPFLAGS -I/usr/local/opt/libxml2/include
 
-set -x PKG_CONFIG_PATH /usr/local/opt/libxml2/lib/pkgconfig
+# You can get openssl-dir to exec 'brew --prefix openssl@1.1'
+set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=/usr/local/opt/openssl@1.1
 
 # alias
 alias git hub
