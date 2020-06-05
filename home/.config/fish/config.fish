cc2bb0280f2b1927f051d1d5d33e4a8ba6a2b3ad
r_takaishi <ryo.takaishi.0@gmail.com>
2020-06-05 17:17:49 +0900

    delete

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -47,26 +47,6 @@ else
   alias diff "diff -u"
 end
 
-if type -q direnv
-  function __direnv_export_eval --on-variable PWD
-    status --is-command-substitution; and return
-    eval (direnv hook fish)
-  end
-else
-  echo "Install direnv first! Check http://direnv.net" ^&1
-end
-
-function history-merge --on-event fish_preexec
-  history --save
-  history --merge
-end
-
-function peco_sync_select_history
-  history-merge
-  peco_select_history $argv
-end
-
-
 function export
     set -l s (string split -m1 = $argv)
     set -xg $s
